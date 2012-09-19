58b0a30754a7fa342bd1569d971a0d9f78f71818
Marat Ibadinov <ibadinov@me.com>
2012-09-19 13:14:24 +0300

    corrected a typo in TargetConditionalsUnix.h

--- Headers/Portability/TargetConditionalsUnix.h
+++ Headers/Portability/TargetConditionalsUnix.h
@@ -7,9 +7,9 @@
 
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__) || defined(_SYSTYPE_BSD)
     #define TARGET_OS_BSD       1
-#if defined(__GNU__)
+#elif defined(__GNU__)
     #define TARGET_OS_HURD      1
-#elif defined(__linux) || define(__linux) || defined(linux)
+#elif defined(__linux) || defined(__linux) || defined(linux)
     #define TARGET_OS_LINUX     1
 #elif defined(__sun) || defined(sun)
     #define TARGET_OS_SOLARIS
